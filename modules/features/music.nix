a16a3d87de3ad9ce127d7211e2c3a4e194675eae
Caio Peluti <cpeluti@gmail.com>
2026-04-06 14:38:28 -0300

    trying to fix beets

--- modules/features/music.nix
+++ modules/features/music.nix
@@ -2,7 +2,9 @@
   flake.homeModules.music = 
     { pkgs, nixpkgs, ... }: {
       nixpkgs.config.allowUnfreePredicate = (pkg: true);
-      home.packages = [pkgs.spotify-player pkgs.spotify pkgs.beets pkgs.python313Packages.beets-alternatives];
+      home.packages = [pkgs.spotify-player pkgs.spotify (pkgs.beets.override {
+        pluginPackages = [ pkgs.python313Packages.beets-alternatives ];
+      })];
       services.spotifyd = {
         enable = true;
         settings = {
