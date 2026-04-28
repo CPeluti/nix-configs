5b72eeaa8561499470b2b1def97e65d9f0f56a5e
Caio Peluti <cpeluti@gmail.com>
2026-04-28 11:28:41 -0300

    added keymap.toml to yazi

--- modules/features/yazi.nix
+++ modules/features/yazi.nix
@@ -5,5 +5,6 @@
       enable = true;
     };
     xdg.configFile."yazi/yazi.toml".source = ../../config/yazi/yazi.toml;
+    xdg.configFile."yazi/keymap.toml".source = ../../config/yazi/keymap.toml;
   };
 }
